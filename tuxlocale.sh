f74e005201ac5237a48e4395d4b3155ed5af98eb
Lucas Pereira Brígida <grillobright@gmail.com>
2011-10-02 15:44:55 -0300

    change Interval value in tuxlocale.sh

--- tuxlocale.sh
+++ tuxlocale.sh
@@ -2,7 +2,7 @@
 
 USERNAME="user"
 PASSWORD="pass"
-UPDATE_INTERVAL=300
+UPDATE_INTERVAL=0
 
 python /opt/tuxlocale/tuxlocale --post $USERNAME $PASSWORD $UPDATE_INTERVAL >> /dev/null &
 echo 'Tux Locale start [ok]'
